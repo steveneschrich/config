0d94a0273190f55d9cf2f3050dbf37bb1356c2d6
Steven Eschrich <35569991+steveneschrich@users.noreply.github.com>
2022-09-02 16:34:52 -0400

    New function get.d which provides a linux-style config.d structure for config files.

---
+++ R/get.d.R
@@ -0,0 +1,28 @@
+#' Get config from multiple individual files
+#'
+#' @description This uses the linux approach of a `config.d` type directory with individual configs to load
+#' and merge.
+#'
+#' @param dir The directory to use for config files.
+#' @param pattern (default *.conf) File pattern for config files.
+#' @param ... Any other parameters to pass to [config::get()].
+#'
+#' @return A configuration list (see [config::get()]).
+#'
+#' @export
+#'
+#' @examples
+#'
+#' \dontrun{
+#' conf <- get.d(dir = "config.d")
+#' }
+get.d <- function(dir, pattern="*.conf", ...) {
+
+  # Load all configuration
+  # We are trying to use package config, which does allow a merge of configuration options.
+  conf <- list.files(dir, pattern=pattern, full.names=TRUE) |>
+    purrr::map(~get(file=., ...)) |>
+    purrr::reduce(merge)
+
+  conf
+}
